cc10163a0c42586a0ed99486e33928bb8a152d77
fin506 <finlayallman7@gmail.com>
2024-11-17 19:03:49 +0000

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     client: {
-        token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '' // ← Your bot ID
+        token: 'MTI2NjAwNjMwNTAyMjQxMDgwMg.Gt34dh.zcNgkT5npD6C7yoQlAGVB_ygk8-2lKVBRtRhRc', // ← Your bot token (.env IS RECOMMENDED)
+        id: '1266006305022410802' // ← Your bot ID
     },
     modmail: {
-        guildId: '', // ← Your server ID
-        categoryId: '', // ← The modmail category ID
-        staffRoles: [''], // ← The modmail staff roles IDs
+        guildId: '1072112392026210414', // ← Your server ID
+        categoryId: '1072112392646951065', // ← The modmail category ID
+        staffRoles: ['1085136129629630515'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
-        webhookURL: '' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
+        webhookURL: 'https://discord.com/api/webhooks/1184944805780922418/04UQJALA9IuAO5idzFoP1va1vGdLBbiNwI15SDqzZ68tjYFkVkfPdoIk6usxzbWcOvB0' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
     }
 };
